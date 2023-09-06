170822049fbf65a69215867e70f0f16ace0f8c45
Ludo Vlieger <lvlieger@twilio.com>
2023-09-06 08:49:15 -0400

    Updated function

--- functions/phoneNumberLookup.protected.js
+++ functions/phoneNumberLookup.protected.js
@@ -2,10 +2,19 @@ exports.handler = (context, event, callback) => {
   const phNumber = event.twilioPhoneNumber;
   console.log('EVENT:', event);
   const phNumbers = {
-    "+18587588840" : {
-      "campaign": "Promotion1"
+    "+18587588840": {
+      "campaign": "Campaign1",
+      "TFN": "8001234567"
+    },
+    "+18123334455": {
+      "campaign": "Campaign2",
+      "TFN": "8005556789"
     }
   };
-  let result = { campaign: phNumbers[phNumber].campaign };
+  let result = {
+    campaign: phNumbers[phNumber].campaign,
+    TFN: phNumbers[phNumber].TFN
+
+  };
   return callback(null, result);
 };
